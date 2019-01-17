b94b9b5776f9630c4c08d457611a3b01e85e9d66
José Miguel Henao Cardona <josem.henao@gmail.com>
2019-01-17 14:01:37 -0500

    Se han agregado las tablas de Ciudades y Departamentos y se está eligiendo la Ciudad a la hora de agregar un Lugar

--- lugares/models.py
+++ lugares/models.py
@@ -18,6 +18,36 @@ class Categoria(models.Model):
         return self.categoria
 
 
+class Departamento(models.Model):
+    id_departamento = models.CharField(max_length=2, null=False, primary_key=True)
+    departamento = models.CharField(max_length=255, null=False, default='')
+
+    class Meta:
+        verbose_name = 'Departamento'
+        verbose_name_plural = "Departamentos"
+        ordering = ['departamento', 'id_departamento']
+
+    def __str__(self):
+        return self.departamento
+
+class Ciudad(models.Model):
+    id_ciudad = models.CharField(max_length=6, primary_key=True)
+    ciudad = models.CharField(max_length=255, default='',
+                              help_text='Nombre de la ciudad')
+    estado = models.CharField(max_length=1,
+                              help_text='Estado actual de la Ciudad, 0:inactivo, 1:activo')
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE,
+                                        help_text='FK al departamento al que se pertenece')
+
+    class Meta:
+        verbose_name = 'Ciudad'
+        verbose_name_plural = "Ciudades"
+        ordering = ['ciudad', 'departamento','estado']
+
+    def __str__(self):
+        return self.ciudad
+
+
 class Lugar(models.Model):
     id_lugar = models.AutoField("Id_lugar", primary_key=True)
 
@@ -40,25 +70,27 @@ class Lugar(models.Model):
     direccion = models.CharField(max_length=150, null=True, blank=True,
                                  help_text="Dirección del Lugar"
                                  )
-    ciudad = models.CharField(max_length=60, null=True, blank=True,
-                              help_text="Ciudad donde se encuentra el Lugar"
-                              )
-    admin = models.ForeignKey(Usuario, on_delete=models.CASCADE, null=True, blank=True)
+    ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE,
+                               help_text='Ciudad donde se ubica el establecimiento')
+
+    admin = models.ForeignKey(Usuario, on_delete=models.CASCADE, null=True, blank=True,
+                              help_text='Usuarios de la plataforma que administra este Lugar')
 
     img_ppal = models.ImageField(upload_to='lugares/img/lugar', null=True, blank=True,
                                  help_text="Imagen principal del Lugar",
                                  default=DEFAULT_LUGAR_IMAGE
                                  )
     imagenes = models.ManyToManyField(to=Imagen, verbose_name="Lista de imagenes", null=True, blank=True)
-    # Verificar la importancia de esto (ver si es mejor reescalar la imagen ppal)
+
+    #  Verificar la importancia de esto (ver si es mejor reescalar la imagen ppal)
     # img_slide = models.ImageField(upload_to='lugares/img/lugar', null=True, blank=True,
     #                               help_text="Imagen de un tamaño 1000 x 330 px, para mostrar en el slide de la"
     #                                         " sección Lugares")
 
     # Revisar la mejor manera almacenar las imagenes de modo que sea 'n' numero de imagenes
-    #img_1 = models.ImageField(upload_to='lugares/img/lugar', null=True, blank=True)
-    #img_2 = models.ImageField(upload_to='lugares/img/lugar', null=True, blank=True)
-    #img_3 = models.ImageField(upload_to='lugares/img/lugar', null=True, blank=True)
+    # img_1 = models.ImageField(upload_to='lugares/img/lugar', null=True, blank=True)
+    # img_2 = models.ImageField(upload_to='lugares/img/lugar', null=True, blank=True)
+    # img_3 = models.ImageField(upload_to='lugares/img/lugar', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Lugar'
@@ -71,3 +103,5 @@ class Lugar(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.nombre)
         super(Lugar, self).save(*args, **kwargs)
+
+
